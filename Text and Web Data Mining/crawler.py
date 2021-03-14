a78707b5915a915fe0fc9a640b187121e0c46aa1
NghiaPH <nghiaphamln3@gmail.com>
2021-03-14 11:12:54 +0700

    sửa code crawl

--- Text and Web Data Mining/crawler.py
+++ Text and Web Data Mining/crawler.py
@@ -1,19 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 
 
-def convert_rating(rating):
-    if rating <= 4:
+# url = "https://www.foody.vn/thuong-hieu/lotteria-ga-ran-sai-gon?c=ho-chi-minh"
+def convert_rating(number):
+    if number <= 4:
         return -1
-    elif rating >= 7:
+    elif number >= 7:
         return 1
     else:
         return 0
 
 
+count = 0
 with open("comments.txt", "w") as write_file:
-    count = 0
 
     with open("urls.txt", "r") as file:
         url = file.readline()
@@ -22,23 +22,25 @@ with open("comments.txt", "w") as write_file:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, "html5lib")
 
-            script = soup.find_all("script")[12].text.split("\n")[3].rstrip().lstrip().replace("var initDataRes=", "")
-            dictionary = json.loads(script[:-1])
+            for x in soup.find_all("h2")[:-1]:
+                response = requests.get(f"https://www.foody.vn{x.a['href']}/binh-luan")
+                soup = BeautifulSoup(response.content, "html5lib")
 
-            # print(json.dumps(dictionary, indent=4, ensure_ascii=False))
+                # for comment in soup.find_all("div", class_="rd-des toggle-height"):
+                #     print(comment.span.text)
 
-            for x in dictionary["Items"]:
-                for i in x["LstReview"]:
+                for y in soup.find_all("div", class_="review-des fd-clearbox")[:-3]:
                     count += 1
+                    comment = y.find_all("div")[1].span.text.replace('. \n', '. ').replace('.\n', '.').replace('\n', '. ')
+                    rating = y.find_all("div")[0].span.text
+                    print(f"train_{count}")
+                    print(comment)
+                    print(rating)
+                    print()
 
-                    # replace icon with positive and negative
-                    text = i['Comment'].replace('. \n', '. ').replace('.\n', '.').replace('\n', '. ')
-                    # lower string and remove stopword
                     write_file.write(f"train_{count}\n")
-                    write_file.write(f'"{text}"\n')
-                    write_file.write(str(convert_rating(i["AvgRating"])) + "\n\n")
+                    write_file.write(f'"{comment}"\n')
+                    write_file.write(str(convert_rating(float(rating))) + "\n\n")
 
-            url = file.readline()
-
-print(f"Đã lấy được {count} bình luận!")
+print(f"Đã lấy được {count} đánh giá!")
 write_file.close()
